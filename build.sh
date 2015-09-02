b64574aaa6db6fdc4152ace1bde113744211b6af
Zizheng Tai <zizheng.tai@gmail.com>
2015-09-01 17:23:41 -0700

    cleaned up build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-rm -f ecumene
 gcc -Wall -Wextra -pedantic -O3 -flto -o ecumene ecumene.c assignment_service.c expiration_service.c heartbeat_service.c redis_script.c -lczmq -lhiredis -ljemalloc
