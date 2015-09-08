81819b65a455157ae37a45b97bf6813489b91f56
Zizheng Tai <zizheng.tai@gmail.com>
2015-09-07 21:44:30 -0700

    moved source files into src folder

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -std=c99 -Wall -Wextra -pedantic -O3 -flto -o ecumene ecumene.c assignment_service.c expiration_service.c heartbeat_service.c redis_script.c -lczmq -lhiredis -ljemalloc
+gcc -std=c99 -Wall -Wextra -pedantic -O3 -flto -o ecumene src/*.c -lczmq -lhiredis -ljemalloc
