e999c9ef8c64ce94912b6b8087ff5fed65bfb663
Zizheng Tai <zizheng.tai@gmail.com>
2015-09-30 18:43:14 -0700

    updated build script for El Capitan

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -std=c99 -Wall -Wextra -pedantic -O3 -flto -o ecumene src/*.c -lczmq -lhiredis -ljemalloc
+gcc -std=c99 -Wall -Wextra -pedantic -O3 -flto -I/usr/local/include -L/usr/local/lib -o ecumene src/*.c -lczmq -lhiredis -ljemalloc
